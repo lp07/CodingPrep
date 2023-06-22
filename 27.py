85fbb191d460989add654be69f90bee3d4c8cc9c
lp07 <lisa.ppatel26@gmail.com>
2023-06-21 22:13:24 -0700

    code prep

---
+++ 27.py
@@ -0,0 +1,34 @@
+# 27. Remove Element
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+# The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+# Consider the number of elements in nums which are not equal to val be k,
+# to get accepted, you need to do the following things:
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+# The remaining elements of nums are not important as well as the size of nums.
+# Return k
+# Example 1:
+# Input: nums = [3,2,2,3], val = 3, Output: 2, nums = [2,2,_,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+# Example 2:
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2, Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+# Note that the five elements can be returned in any order.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+
+class Solution:
+    def removeElement(self, nums, val) -> int:
+        i = 0
+        for n in range(len(nums)):
+            # current element is equals to the given value
+            if nums[n] == val:
+                continue  # if it is equals, skip the remaining code inside the loop
+            # if it is not equals, assign to the position i
+            nums[i] = nums[n]
+            i += 1  # increment the value of i by 1
+        return i
+solution = Solution()
+result = solution.removeElement([3,2,2,3], 3)
+print(result)
+
+# output: 2
